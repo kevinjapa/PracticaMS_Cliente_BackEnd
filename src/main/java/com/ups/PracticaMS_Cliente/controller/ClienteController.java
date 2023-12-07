897749f99f83317b8c9c8bc9a8281f1ddfcb6190
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-12-07 00:03:15 -0500

    creacion de y actualizacion de Repository y Services

--- src/main/java/com/ups/PracticaMS_Cliente/controller/ClienteController.java
+++ src/main/java/com/ups/PracticaMS_Cliente/controller/ClienteController.java
@@ -1,11 +1,39 @@
 package com.ups.PracticaMS_Cliente.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ups.PracticaMS_Cliente.model.Cliente;
+import com.ups.PracticaMS_Cliente.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/MS1/Cliente")
 
-
 public class ClienteController {
+
+   @Autowired
+   public ClienteService clienteService;
+
+   // Para consumir el Guardar Clientes desde Postman o FronEnd
+   public ResponseEntity<Cliente> save(@RequestBody Cliente cliente){
+       Cliente t= clienteService.create(cliente);
+
+       try {
+            return ResponseEntity.created(new URI("/MS1/Cliente/guardar"+t.getCedula())).body(t);
+       }
+       catch (Exception ex)
+       {
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+       }
+   }
+
+   //Para Consumir la lista de Clientes
+    @GetMapping("/all")
+    public ResponseEntity<List<Cliente>> listAll(){
+       return ResponseEntity.ok(clienteService.listClientes());
+    }
 }
--- src/main/java/com/ups/PracticaMS_Cliente/services/ClienteService.java
+++ src/main/java/com/ups/PracticaMS_Cliente/services/ClienteService.java
@@ -1,18 +1,25 @@
 package com.ups.PracticaMS_Cliente.services;
 
 
+import com.ups.PracticaMS_Cliente.model.Cliente;
 import com.ups.PracticaMS_Cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
 
     @Autowired
     private ClienteRepository estudianteRepository;
-
+    // Guardar mis Clientes
     public Cliente create(Cliente cliente){
         return estudianteRepository.save(cliente);
-        
+    }
+
+    // Listar mis Clientes
+    public List<Cliente> listClientes(){
+        return estudianteRepository.findAll();
     }
 }
