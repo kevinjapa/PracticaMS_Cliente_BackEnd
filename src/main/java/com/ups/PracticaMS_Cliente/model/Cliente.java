328478f68188d08df6d19026dcbc2cf7afaec7a5
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-12-06 22:27:13 -0500

    CCliente
    Creacion de la clase cliente

---
+++ src/main/java/com/ups/PracticaMS_Cliente/model/Cliente.java
@@ -0,0 +1,52 @@
+package com.ups.PracticaMS_Cliente.model;
+
+public class Cliente {
+
+    public String cedula;
+
+    public String nombre;
+    public String apellido;
+    public String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(String cedula, String nombre, String apellido, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
